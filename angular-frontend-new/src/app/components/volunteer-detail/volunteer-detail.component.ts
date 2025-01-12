899bdec49b0f5c51becbd85c71a5002f3276376e
Tobias Hinum <t.hinum@gmx.at>
2025-01-12 16:56:33 +0100

    Added structure to new angular part

---
+++ angular-frontend-new/src/app/components/volunteer-detail/volunteer-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-detail',
+  imports: [],
+  templateUrl: './volunteer-detail.component.html',
+  styleUrl: './volunteer-detail.component.css'
+})
+export class VolunteerDetailComponent {
+
+}
