5ee605c277f9e4571183252228a5aff84a366392
Tobias Hinum <t.hinum@gmx.at>
2025-02-09 16:47:46 +0100

    Fixed Stuff

--- angular-frontend-new/src/app/app.routes.ts
+++ angular-frontend-new/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import {UserProfileComponent} from './components/user/user-profile/user-profile.component';
+import {
+  OrganisationsOverviewComponent
+} from './components/organisations/organisations-overview/organisations-overview.component';
 import {
   OrganisationDetailComponent
 } from './components/organisations/organisation-detail/organisation-detail.component';
-import {UserProfileComponent} from './components/user/user-profile/user-profile.component';
 
 export const routes: Routes = [
-  {path: '', component: UserProfileComponent},
-  {path: 'organisations', component: OrganisationDetailComponent}
+  {path: '', component: UserProfileComponent, data: {title: 'Profil'}},
+  {path: 'organisations', component: OrganisationsOverviewComponent, data: {title: 'Organisation Overview'}},
+  {path: 'organisation', component: OrganisationDetailComponent, data: {title: 'Organisation'}}
+  /**TODO change organisations to single organisation just for testing purposes for now */
 ]
--- angular-frontend-new/src/app/shared/components/header/header.component.ts
+++ angular-frontend-new/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {filter, map} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +12,31 @@ import {MatToolbar} from '@angular/material/toolbar';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Input() title: string | undefined;
+  @Input() title : string = ""
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(){
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routeTitle = '';
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+          if (route.snapshot.data['title']) {
+            routeTitle = route!.snapshot.data['title'];
+          }
+          return routeTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.title = title
+        }
+      });
+  }
 }
