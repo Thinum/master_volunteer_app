0c37964a3988e048bf41e927f4c565faaf64d4af
Tobias Hinum <t.hinum@gmx.at>
2025-10-26 20:55:28 +0100

    Add login rout and auth checking to other routes

--- angular-frontend-new/src/app/app.component.ts
+++ angular-frontend-new/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, computed, Signal, signal} from '@angular/core';
 import {NavBarComponent} from './shared/components/nav-bar/nav-bar.component';
 import {HeaderComponent} from './shared/components/header/header.component';
 import {RouterOutlet} from '@angular/router';
+import {LayoutingService} from './services/layouting.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,11 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-frontend-new';
+  protected showBottomNavbar: Signal<boolean> = signal(true);
+
+  constructor(layoutingService: LayoutingService) {
+    this.showBottomNavbar = computed(() => {
+      return layoutingService.showBottomNavbar()
+    });
+  }
 }
--- angular-frontend-new/src/app/app.routes.ts
+++ angular-frontend-new/src/app/app.routes.ts
@@ -15,17 +15,20 @@ import {
 } from './components/activities/activity-detail/activity-detail.component';
 import {CommunityComponent} from './components/community/community.component';
 import {ProjectDetailComponent} from './components/project/project-detail/project-detail.component';
+import {authGuard} from './guard/authGuard';
+import {LoginComponent} from './components/login/login.component';
 
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent, data: {title: 'Home'}},
-  {path: 'organisations', component: OrganisationsOverviewComponent, data: {title: 'Organisation Overview'}},
-  {path: 'organisations/:id', component: OrganisationDetailComponent, data: {title: 'Organisation'}},
-  {path: 'community', component: CommunityComponent, data: {title: 'Community'}},
+  {path: '', component: LoginComponent, data: {title: 'Login'}},
+  {path: 'home', component: HomeComponent, data: {title: 'Home'}, canActivate: [authGuard]},
+  {path: 'organisations', component: OrganisationsOverviewComponent, data: {title: 'Organisation Overview'}, canActivate: [authGuard]},
+  {path: 'organisations/:id', component: OrganisationDetailComponent, data: {title: 'Organisation'}, canActivate: [authGuard]},
+  {path: 'community', component: CommunityComponent, data: {title: 'Community'}, canActivate: [authGuard]},
   /**TODO change organisations to single organisation just for testing purposes for now */
-  {path: 'profile', component: UserProfileComponent, data: {title: 'Profil'}},
-  {path: 'profile/:id', component: UserProfileComponent, data: {title: 'Profil'}},
-  {path: 'activities', component: ActivityOverviewComponent, data: {title: 'Activities'}},
-  { path: 'activities/:id', component: ActivityDetailComponent, data: {title: 'Activities'}},
-  { path: 'projects/:id', component: ProjectDetailComponent, data: {title: 'Projects'} }
+  {path: 'profile', component: UserProfileComponent, data: {title: 'Profil'}, canActivate: [authGuard]},
+  {path: 'profile/:id', component: UserProfileComponent, data: {title: 'Profil'}, canActivate: [authGuard]},
+  {path: 'activities', component: ActivityOverviewComponent, data: {title: 'Activities'}, canActivate: [authGuard]},
+  { path: 'activities/:id', component: ActivityDetailComponent, data: {title: 'Activities'}, canActivate: [authGuard]},
+  { path: 'projects/:id', component: ProjectDetailComponent, data: {title: 'Projects'}, canActivate: [authGuard] }
 ]
