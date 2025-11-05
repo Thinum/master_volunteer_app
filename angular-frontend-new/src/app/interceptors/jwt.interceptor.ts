821ee2ca4e75700cb367acb2a6756abd2b237a2b
ManuelVujakovic <42140779+ManuelVujakovic@users.noreply.github.com>
2025-11-05 13:54:47 +0100

    ADDED: auto logout if token expires and cleaned bit of logic

--- angular-frontend-new/src/app/interceptors/jwt.interceptor.ts
+++ angular-frontend-new/src/app/interceptors/jwt.interceptor.ts
@@ -1,15 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import {AUTH_TOKEN_STRING} from '../services/authservice/auth.service';
+import { AUTH_TOKEN_STRING, EXPIRY_DATE_STRING } from '../services/authservice/auth.service';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = localStorage.getItem(AUTH_TOKEN_STRING);
+  const expiry = localStorage.getItem(EXPIRY_DATE_STRING);
 
-  if(authToken){
-    const clonedRequest = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + authToken)
-    })
-    return next(clonedRequest);
-  } else {
-    return next(req);
+  const isValid = authToken && expiry && new Date().getTime() < new Date(expiry).getTime();
+
+  if (isValid) {
+    const cloned = req.clone({
+      setHeaders: { Authorization: `Bearer ${authToken}` }
+    });
+    return next(cloned);
   }
+
+  return next(req);
 };
--- angular-frontend-new/src/app/services/authservice/auth.service.ts
+++ angular-frontend-new/src/app/services/authservice/auth.service.ts
@@ -1,54 +1,77 @@
-import {EventEmitter, Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {AuthUserDTOModel} from '../../models/contract/AuthUserDTO.model';
-import {AuthToken} from '../../models/contract/AuthToken.model';
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthUserDTOModel } from '../../models/contract/AuthUserDTO.model';
+import { AuthToken } from '../../models/contract/AuthToken.model';
 
-// TODO: Maybe move into own seperate constant class?
-export const EXPIRY_DATE_STRING: string= "expiryDate";
-export const AUTH_TOKEN_STRING: string= "authToken";
+export const EXPIRY_DATE_STRING = 'expiryDate';
+export const AUTH_TOKEN_STRING = 'authToken';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public loggedInEvent = new EventEmitter<boolean>();
+  private tokenCheckInterval: any;
 
-  public loggedInEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     if (this.isLoggedIn()) {
       this.loggedInEvent.emit(true);
+      this.startTokenCheck();
+    } else {
+      this.logout();
     }
   }
 
+  public login(username: string, password: string) {
+    const body: AuthUserDTOModel = { username, password };
+    this.http.post<AuthToken>('http://localhost:8080/auth/login', body)
+      .subscribe({
+        next: (authToken) => {
+          this.setSession(authToken);
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+          this.loggedInEvent.emit(false);
+        }
+      });
+  }
+
+  public logout() {
+    localStorage.removeItem(AUTH_TOKEN_STRING);
+    localStorage.removeItem(EXPIRY_DATE_STRING);
+    this.loggedInEvent.emit(false);
+    this.router.navigate(['']);
+    if (this.tokenCheckInterval) clearInterval(this.tokenCheckInterval);
+  }
+
+  public isLoggedIn(): boolean {
+    return new Date().getTime() < this.getExpiration().getTime();
+  }
 
   public isAuthenticated(){
     return this.isLoggedIn();
   }
 
-  public login(userName:string, password:string){
-    const postArg: AuthUserDTOModel = {username : userName, password: password}
-    this.http.post<AuthToken>('http://localhost:8080/auth/login', postArg)
-      .subscribe(authToken => this.setSession(authToken));
-  }
-
-  public logout(){
-    localStorage.removeItem(AUTH_TOKEN_STRING);
-    localStorage.removeItem(EXPIRY_DATE_STRING);
-    this.loggedInEvent.emit(false);
-  }
-
-  public isLoggedIn(){
-    return new Date().getTime() < this.getExpiration().getTime();
-  }
-
-  private setSession(authToken: AuthToken){
+  private setSession(authToken: AuthToken) {
     localStorage.setItem(AUTH_TOKEN_STRING, authToken.token);
     localStorage.setItem(EXPIRY_DATE_STRING, authToken.expiryDate.toString());
     this.loggedInEvent.emit(true);
+    this.startTokenCheck();
   }
 
   private getExpiration(): Date {
     const expiration = localStorage.getItem(EXPIRY_DATE_STRING);
-    return expiration ? new Date(expiration) : new Date(0); // epoch = expired
+    return expiration ? new Date(expiration) : new Date(0);
+  }
+
+  private startTokenCheck() {
+    if (this.tokenCheckInterval) clearInterval(this.tokenCheckInterval);
+
+    this.tokenCheckInterval = setInterval(() => {
+      if (!this.isLoggedIn()) {
+        this.logout();
+      }
+    }, 5000);
   }
 }
