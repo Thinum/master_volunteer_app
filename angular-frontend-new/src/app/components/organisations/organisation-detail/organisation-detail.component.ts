1fc07831be25338d4148b2e7b4089a10349e2220
ManuelVujakovic <42140779+ManuelVujakovic@users.noreply.github.com>
2025-11-16 19:38:58 +0100

    CHANGED: Organisation layout etc, the header is still wrong

--- angular-frontend-new/src/app/components/organisations/organisation-detail/organisation-detail.component.ts
+++ angular-frontend-new/src/app/components/organisations/organisation-detail/organisation-detail.component.ts
@@ -1,19 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
-import {MatProgressBar} from '@angular/material/progress-bar';
-import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router';
-import {OrganisationService} from '../../../services/api/organisation.service';
-import {NgIf} from '@angular/common';
-import {CardComponent} from '../../../shared/components/card/card.component';
+import { Component, OnInit } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatListModule } from '@angular/material/list';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { OrganisationService } from '../../../services/api/organisation.service';
+import { VolunteerService } from '../../../services/api/volunteer.service';
+import { NgIf } from '@angular/common';
+import { CardComponent } from '../../../shared/components/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { User } from '../../../models/user.model'
 
 @Component({
   selector: 'app-organisation-detail',
   imports: [
     MatIcon,
     MatProgressBar,
+    MatListModule,
+    MatCardModule,
+    DatePipe,
+    CommonModule,
     NgIf,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
   ],
   templateUrl: './organisation-detail.component.html',
   styleUrl: './organisation-detail.component.css'
@@ -21,14 +31,18 @@ import {CardComponent} from '../../../shared/components/card/card.component';
 export class OrganisationDetailComponent implements OnInit{
   detailedOrganisation: any;
   private id?: number | null;
-  constructor(private route: ActivatedRoute, private organisationService: OrganisationService) {
-
-  }
+  friends: User[] = [];
+  constructor(private route: ActivatedRoute, private organisationService: OrganisationService,  private volunteerService: VolunteerService) {}
 
   ngOnInit(){
     this.id = parseInt(this.route.snapshot.paramMap.get('id') ?? '-1');
     if(this.id) {
       this.organisationService.getOrganisationById(this.id).subscribe(org => this.detailedOrganisation=org)
     }
+
+    this.volunteerService.getAllVolunteers().subscribe(users => {
+      this.friends = users;
+    });
+
   }
 }
