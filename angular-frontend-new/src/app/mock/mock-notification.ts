27623e82714e4e212c75ea4b9afcdb9640ca6440
Tobias Hinum <t.hinum@gmx.at>
2025-11-23 14:46:57 +0100

    Added notifications mock

---
+++ angular-frontend-new/src/app/mock/mock-notification.ts
@@ -0,0 +1,9 @@
+import {AppNotification} from '../models/notification.model';
+
+export const MOCK_NOTIFICATIONS: AppNotification[] = [
+  {
+    id: 1,
+    type: 'friend_request',
+    message: 'Someone sent you a friend request'
+  }
+];
---
+++ angular-frontend-new/src/app/models/notification.model.ts
@@ -0,0 +1,5 @@
+export interface AppNotification {
+  id: number;
+  type: string; //remove for enum
+  message: string;
+}
