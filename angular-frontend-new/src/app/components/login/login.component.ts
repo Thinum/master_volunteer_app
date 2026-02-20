7e956d23b9e7a00063837e60020eb0f2f4121843
ManuelVujakovic <42140779+ManuelVujakovic@users.noreply.github.com>
2026-02-20 19:48:38 +0100

    Added Organisation search to login

--- angular-frontend-new/src/app/components/login/login.component.ts
+++ angular-frontend-new/src/app/components/login/login.component.ts
@@ -1,67 +1,101 @@
-import {Component, OnDestroy, signal} from '@angular/core';
-import {LayoutingService} from '../../services/layouting.service';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {MatInput} from '@angular/material/input';
-import {MatIcon} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {AuthService} from '../../services/authservice/auth.service';
-import {Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import { Component, OnInit, OnDestroy, signal } from '@angular/core';
+import { LayoutingService } from '../../services/layouting.service';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/authservice/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { OrganisationListComponent } from '../organisations/organisations-overview/organisation-list/organisation-list.component';
+import { Organisation } from '../../models/organisation.model';
+import { OrganisationService } from '../../services/api/organisation.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     MatFormField,
     MatLabel,
     MatInput,
     MatIcon,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OrganisationListComponent
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnDestroy{
+export class LoginComponent implements OnInit, OnDestroy {
+
+  organisations: Organisation[] = [];
+  organisationsFiltered: Organisation[] = [];
 
   protected loginForm: FormGroup;
-
   protected hidePw = signal(true);
 
-  private loggedInSubscription: Subscription;
+  private loggedInSubscription?: Subscription;
 
-
-  constructor(private formbuilder: FormBuilder,
-              private layoutingService: LayoutingService,
-              private authService: AuthService,
-              private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private layoutingService: LayoutingService,
+    private authService: AuthService,
+    private router: Router,
+    private organisationService: OrganisationService
+  ) {
     this.layoutingService.showBottomNavbar.set(false);
-    this.loginForm = this.formbuilder.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-    })
-    this.loggedInSubscription = this.authService.loggedInEvent.subscribe(loggedInEvent => {
-      if (loggedInEvent) {
-        this.layoutingService.showBottomNavbar.set(true);
-        this.router.navigate(['home']);
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.loggedInSubscription = this.authService.loggedInEvent.subscribe(
+      loggedInEvent => {
+        if (loggedInEvent) {
+          this.layoutingService.showBottomNavbar.set(true);
+          this.router.navigate(['home']);
+        }
       }
-    })
+    );
   }
 
-  ngOnDestroy(): void {
-        this.loggedInSubscription.unsubscribe();
+  ngOnInit(): void {
+    this.loadOrganisations();
   }
 
-  clickHidePw(event: MouseEvent) {
-    this.hidePw.set(!this.hidePw());
+  filterBy(searchTerm: string): void {
+    if (searchTerm) {
+      const lower = searchTerm.toLowerCase();
+      this.organisationsFiltered = this.organisations.filter(o =>
+        o.orgName.toLowerCase().includes(lower)
+      );
+    } else {
+      this.organisationsFiltered = [...this.organisations];
+    }
+  }
+
+  private loadOrganisations(): void {
+    this.organisationService.getAllOrganisations().subscribe(orgs => {
+      this.organisations = orgs;
+      this.organisationsFiltered = [...orgs];
+    });
+  }
+
+  clickHidePw(event: MouseEvent): void {
+    this.hidePw.update(v => !v);
     event.stopPropagation();
   }
 
-  clickLogin() {
-    const formValue = this.loginForm.value;
+  clickLogin(): void {
+    if (this.loginForm.invalid) return;
 
-    if(formValue.username && formValue.password){
-      this.authService.login(formValue.username, formValue.password)
-    }
+    const { username, password } = this.loginForm.value;
+    this.authService.login(username, password);
+  }
+
+  ngOnDestroy(): void {
+    this.loggedInSubscription?.unsubscribe();
   }
 }
