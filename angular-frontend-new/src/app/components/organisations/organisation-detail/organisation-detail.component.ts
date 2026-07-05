c8296b340cbbf41a9e7b955fb84642bd2e2db794
ManuelVujakovic <42140779+ManuelVujakovic@users.noreply.github.com>
2026-07-05 16:50:22 +0200

    Changed Org Detail to lookup city and added small map to see place

--- angular-frontend-new/src/app/components/organisations/organisation-detail/organisation-detail.component.ts
+++ angular-frontend-new/src/app/components/organisations/organisation-detail/organisation-detail.component.ts
@@ -3,10 +3,10 @@ import { MatIcon } from '@angular/material/icon';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatListModule } from '@angular/material/list';
 import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { OrganisationService } from '../../../services/api/organisation.service';
 import { VolunteerService } from '../../../services/api/volunteer.service';
 import { NgIf } from '@angular/common';
-import { CardComponent } from '../../../shared/components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { DatePipe } from '@angular/common';
@@ -14,8 +14,31 @@ import { User } from '../../../models/user.model'
 import { Router } from '@angular/router';
 import { CommunityGoalService } from '../../../services/api/community-goal.service';
 import { CommunityGoal } from '../../../models/community-goal.model';
-import {MatButton, MatIconButton} from '@angular/material/button';
-import {Organisation} from '../../../models/organisation.model';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { Organisation } from '../../../models/organisation.model';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+const FALLBACK_LOCATION_LABEL = 'Location unavailable';
+const NOMINATIM_REVERSE_URL = 'https://nominatim.openstreetmap.org/reverse';
+
+interface NominatimAddress {
+  house_number?: string;
+  road?: string;
+  pedestrian?: string;
+  footway?: string;
+  city?: string;
+  town?: string;
+  village?: string;
+  municipality?: string;
+  suburb?: string;
+  county?: string;
+  state?: string;
+}
+
+interface NominatimReverseResponse {
+  display_name?: string;
+  address?: NominatimAddress;
+}
 
 @Component({
   selector: 'app-organisation-detail',
@@ -31,23 +54,28 @@ import {Organisation} from '../../../models/organisation.model';
     RouterLinkActive,
     MatIconButton,
     MatButton,
+    GoogleMapsModule,
   ],
   templateUrl: './organisation-detail.component.html',
   styleUrl: './organisation-detail.component.css'
 })
 export class OrganisationDetailComponent implements OnInit {
-detailedOrganisation?: Organisation;
-id?: number | null;
-friends: User[] = [];
-goals: CommunityGoal[] = [];
-hasJoined: boolean = false;
+  detailedOrganisation?: Organisation;
+  id?: number | null;
+  friends: User[] = [];
+  goals: CommunityGoal[] = [];
+  hasJoined = false;
+  locationLabel = FALLBACK_LOCATION_LABEL;
+  mapCenter: google.maps.LatLngLiteral = { lat: 48.3069, lng: 14.2858 };
+  mapMarker?: google.maps.LatLngLiteral;
 
-constructor(
+  constructor(
     private route: ActivatedRoute,
     private organisationService: OrganisationService,
     private volunteerService: VolunteerService,
     private router: Router,
-    private communityGoalService: CommunityGoalService
+    private communityGoalService: CommunityGoalService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
@@ -57,6 +85,8 @@ constructor(
       this.organisationService.getOrganisationById(this.id)
         .subscribe(org => {
           this.detailedOrganisation = org;
+          this.updateMapLocation(org);
+          this.updateLocationLabel(org);
           this.volunteerService.getCurrentUser().subscribe(user => {
             this.hasJoined = this.detailedOrganisation?.orgMembers?.
               some((orgMember) => orgMember.user.id === user.id) ?? false;
@@ -66,7 +96,7 @@ constructor(
       this.communityGoalService.getGoalsForOrganisation(this.id)
         .subscribe(goals => this.goals = goals);
 
-   }
+    }
 
     this.volunteerService.getAllVolunteers()
       .subscribe(users => {
@@ -93,7 +123,7 @@ constructor(
     return (current / target) * 100;
   }
 
-  joinOrganisation(){
+  joinOrganisation() {
     if (this.id === null || this.id === undefined) return;
     this.organisationService.joinOrganisation(this.id).subscribe({
         next: result => {
@@ -109,5 +139,54 @@ constructor(
           console.error('Could not join activity', err)
       },
     );
+  }
+
+  private updateLocationLabel(org: Organisation): void {
+    this.locationLabel = FALLBACK_LOCATION_LABEL;
+
+    const params = new HttpParams()
+      .set('format', 'jsonv2')
+      .set('addressdetails', '1')
+      .set('zoom', '18')
+      .set('layer', 'address')
+      .set('lat', org.location.lat)
+      .set('lon', org.location.lon);
+
+    this.http.get<NominatimReverseResponse>(NOMINATIM_REVERSE_URL, { params })
+      .subscribe({
+        next: result => {
+          this.locationLabel = this.formatNominatimAddress(result);
+        },
+        error: () => {
+          this.locationLabel = FALLBACK_LOCATION_LABEL;
+        },
+      });
+  }
+
+  private updateMapLocation(org: Organisation): void {
+    this.mapCenter = {
+      lat: org.location.lat,
+      lng: org.location.lon,
+    };
+    this.mapMarker = this.mapCenter;
+  }
+
+  private formatNominatimAddress(result: NominatimReverseResponse): string {
+    const address = result.address;
+    if (!address) {
+      return result.display_name || FALLBACK_LOCATION_LABEL;
+    }
+
+    const streetName = address.road || address.pedestrian || address.footway;
+    const street = [streetName, address.house_number].filter(Boolean).join(' ');
+    const city = address.city
+      || address.town
+      || address.village
+      || address.municipality
+      || address.suburb
+      || address.county
+      || address.state;
+
+    return [street, city].filter(Boolean).join(', ') || result.display_name || FALLBACK_LOCATION_LABEL;
   }
 }
