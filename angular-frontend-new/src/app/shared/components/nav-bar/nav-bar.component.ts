6c6b428a2f9430a14593b5e6e02a29ac5e5af94f
Manuel Vujakovic <manuelvujak@gmail.com>
2026-07-17 10:54:48 +0200

    changes for optimized mobile version

--- angular-frontend-new/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ angular-frontend-new/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -19,6 +19,7 @@ import { OrganisationAdminAssignmentService } from '../../../services/api/organi
 })
 export class NavBarComponent implements OnInit {
   hasPlatformAdminAccess = false;
+  isMobileMenuOpen = false;
 
   constructor(private readonly assignmentService: OrganisationAdminAssignmentService) {}
 
@@ -27,5 +28,25 @@ export class NavBarComponent implements OnInit {
       next: hasAccess => this.hasPlatformAdminAccess = hasAccess,
       error: () => this.hasPlatformAdminAccess = false
     });
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    this.closeMobileMenu();
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (window.innerWidth > 600) {
+      this.closeMobileMenu();
+    }
   }
 }
